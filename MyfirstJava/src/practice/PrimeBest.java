f82cec128910f9b43646cb3d973ae8eb2c340bd7
abc <abc@example.com>
2022-09-01 10:52:30 +0530

    recursion

---
+++ MyfirstJava/src/practice/PrimeBest.java
@@ -0,0 +1,55 @@
+package practice;
+
+
+//recursion:
+//calling the same task till the process completes
+//Java program to check whether a number
+//is prime or not using recursion
+class PrimeBest{
+	
+static int i = 2;
+
+//Function check whether a number
+//is prime or not
+public static boolean isPrime(int n)
+{
+	
+	// Corner cases
+	if (n == 0 || n == 1)
+	{
+		return false;
+	}
+
+	// Checking Prime
+	if (n == i)
+		return true;
+		
+	// Base cases
+	if (n % i == 0)
+	{
+		return false;
+	}
+	i++;
+	return isPrime(n);//recursion: calling one module with start and end
+}
+
+//Driver Code
+public static void main(String[] args)
+{
+	//o(n)-o(1)
+	//for(int i=0;i<n;i++) 
+	//{
+	if (isPrime(34))
+	{
+		System.out.println("true");
+	}
+	else
+	{
+		System.out.println("false");
+	}
+//}
+}
+}
+
+//This code is contributed by divyeshrabadiya07
+
