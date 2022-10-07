2fddc82c77783da247873ca5f1322be514886ff6
abc <abc@example.com>
2022-10-07 15:12:50 +0530

    done

---
+++ MyfirstJava/src/practice/TestFinal.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class TestFinal {
+	
+
+	public static void main(String[] args) {
+		final int i=10;//final is a modifier : that changes the nature of the object
+		// TODO Auto-generated method stub
+		//i=? variable/object : TestFinal,initialized to 10. integer
+		System.out.println(i);
+	}
+
+}
