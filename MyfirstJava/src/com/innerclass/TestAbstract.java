6b907274913a17c006e09d6e12b404e42b66f6f5
abc <abc@example.com>
2022-12-06 21:16:49 +0530

    commit

--- MyfirstJava/src/com/innerclass/TestAbstract.java
+++ MyfirstJava/src/com/innerclass/TestAbstract.java
@@ -1,9 +1,12 @@
 package com.innerclass;
 
-public abstract class TestAbstract {
+public abstract class TestAbstract //incomplete class by a concrete class(complete)
+{
 	
-	abstract void m1();
-	public abstract class TestAbstract2{
+	abstract void m1();//at least one abstract method
+	
+	public abstract class TestAbstract2
+	{
 		abstract void m2();
 	}
 
---
+++ MyfirstJava/src/com/innerclass/UseAbstractclass.java
@@ -0,0 +1,17 @@
+package com.innerclass;
+
+//concrete class :UseAbstractclass
+public class UseAbstractclass extends TestAbstract {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	void m1() {
+		// TODO Auto-generated method stub
+		System.out.println("a");//do something
+	}
+
+}
